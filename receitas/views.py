3225b0c694319f3cdcc1bd475698160e099f62b2
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-04-25 13:54:14 -0300

    Colocando templates temp

--- receitas/views.py
+++ receitas/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render # essa função renderiza arquivos
 from django.http import HttpResponse
 
 def Home(request):
-    return render(request, 'receitas/home.html')
+    return render(request, 'receitas/home.html', context= {
+        'nome': 'Guilherme'
+    })
 
 def Sobre(request):
-    return HttpResponse('Sobre')
+    return render(request, 'me_apague/temp.html')
 
 def Contato(request):
     return HttpResponse('Contato')
